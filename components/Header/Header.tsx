de70dae7349b6d9620b85e5d833819893f859c23
Matheus Fabris <13111055+SeikiMatt@users.noreply.github.com>
2021-07-04 20:06:38 -0300

    Improve Header element layout

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -5,17 +5,19 @@ export default function Header() {
         <header className={css.main}>
             <div className={`${css.inner} max-width`}>
                 <div className={css.content}>
-                    <h1>All your files in one secure location, accessible anywhere.</h1>
-                    <p>
+                    <h1 className={css.title}>
+                        All your files in one secure location, accessible anywhere.
+                    </h1>
+                    <p className={css.flavorText}>
                         Fylo stores your most important files in one secure location. Access them
                         wherever you need, share and collaborate with friends, family, and
                         co-workers.
                     </p>
                     <input
-                        className='input input-primary'
+                        className={`input input-primary ${css.input}`}
                         placeholder='Enter your email...'
                     ></input>
-                    <button className='button button-primary'>Get Started</button>
+                    <button className={`button button-primary ${css.button}`}>Get Started</button>
                 </div>
                 <div className={css.illustration}>
                     <img src='illustration-1.svg' />
